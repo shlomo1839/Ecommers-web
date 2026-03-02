47a9d25587b395669955b09082bb3d19c27b22d4
shlomo1839@gmail.com <shlomoelias1830>
2026-03-02 12:15:10 +0200

    added productCard: all product have props of this object, and functions to actions by status, if product already exsists have only remove func, if not add funct

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,20 @@
 import useCartStore from '../store/useCartStore';
 
 
+export const ProductCard = ({ product }) => {
+  const { cart, addToCart, removeFromCart } = useCartStore();
+  const isInCart = cart.find((item) => item.id === product.id);
+
+  return (
+    <div>
+      <div>{product.image}</div>
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+
+      <div>
+        {isInCart ? ( <button onClick={() => removeFromCart(product.id)}>Remove</button>)
+        : <botton onClick={() => addToCart(product)}>Add to cart</botton>}
+      </div>
+    </div>
+  )
+}
